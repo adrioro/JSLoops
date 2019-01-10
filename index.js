f68050fe79207d882924c34fed00dbc306fa8a95
adrioro <artistsvillage@me.com>
2019-01-10 15:40:17 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var MyArray = new Array("Adriana", "Monica", "Patricia");
+
+for (v in MyArray) {
+  document.write("Sudent: " + MyArray[v] + "<br>");
+}
+
+var SecondArray = (["9", "7", "5", "10"], ["Sonia", "John", "Adam", "Jorge"]);
+
+for (d in SecondArray[i]) {
+  document.write(SecondArray[i][d]);
+}
