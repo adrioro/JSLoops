21797e6906e0e8c1ec9bd97e302dfe888c87a1a2
adrioro <artistsvillage@me.com>
2019-01-10 17:56:10 -0500

    third commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ for (v in MyArray) {
   document.write("Sudent: " + MyArray[v] + "<br>");
 }
 
-var SecondArray = (["9", "7", "5", "10"], ["Sonia", "John", "Adam", "Jorge"]);
+var SecondArray = ["a", "b", "c", "d"];
 
-for (d in SecondArray[i]) {
-  document.write(SecondArray[i][d]);
+for (v in SecondArray) {
+  document.write(v + SecondArray[v] + "<br>");
 }
